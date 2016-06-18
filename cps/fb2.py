8a9c97bf69856ab8331dd16d51a581496fab9d0e
Pavel Yakunin <pyakunin@gmail.com>
2016-06-18 16:51:16 +0300

    fb2 uploading

--- cps/fb2.py
+++ cps/fb2.py
@@ -22,35 +22,6 @@ def get_fb2_info(tmp_file_path, original_file_name, original_file_extension):
     title = unicode(tree.xpath('/fb:FictionBook/fb:description/fb:title-info/fb:book-title/text()', namespaces=ns)[0])
     description = unicode(tree.xpath('/fb:FictionBook/fb:description/fb:publish-info/fb:book-name/text()', namespaces=ns)[0])
 
-    #
-    #
-    #
-    # cfname = tree.xpath('n:rootfiles/n:rootfile/@full-path',namespaces=ns)[0]
-    #
-    # cf = zip.read(cfname)
-    # tree = etree.fromstring(cf)
-    #
-    # p = tree.xpath('/pkg:package/pkg:metadata',namespaces=ns)[0]
-    #
-    # epub_metadata = {}
-    # for s in ['title', 'description', 'creator']:
-    #     tmp = p.xpath('dc:%s/text()'%(s),namespaces=ns)
-    #     if (len(tmp) > 0):
-    #         epub_metadata[s] = p.xpath('dc:%s/text()'%(s),namespaces=ns)[0]
-    #     else:
-    #         epub_metadata[s] = "Unknown"
-    #
-    # coversection = tree.xpath("/pkg:package/pkg:manifest/pkg:item[@id='cover']/@href",namespaces=ns)
-    # if (len(coversection) > 0):
-    #     coverfile = extractCover(zip, coversection[0], tmp_file_path)
-    # else:
-    #     coverfile = None
-    # if epub_metadata['title'] is None:
-    #     title = original_file_name
-    # else:
-    #     title = epub_metadata['title']
-    #
-    #
     return uploader.BookMeta(
         file_path = tmp_file_path,
         extension = original_file_extension,
