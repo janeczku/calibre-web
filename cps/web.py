4a2f67bc00676d323f16e3fc42576a77cd317dea
idalin <dalin.lin@gmail.com>
2017-01-16 16:26:20 +0800

    fixed url_for return without port when port is not the standard port.

--- cps/web.py
+++ cps/web.py
@@ -250,7 +250,7 @@ class Pagination(object):
 def url_for_other_page(page):
     args = request.view_args.copy()
     args['page'] = page
-    return url_for(request.endpoint, **args)
+    return url_for(request.endpoint, _external=True, **args)
 
 
 app.jinja_env.globals['url_for_other_page'] = url_for_other_page
