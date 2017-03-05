d06f7979c9aa9d023e97566cecea9d7b0ede88cf
林檎 <apple@apples-MacBook-Pro.local>
2017-03-05 23:01:34 +0800

    byebye vendor lib

--- vendor/sqlalchemy/testing/suite/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from sqlalchemy.testing.suite.test_ddl import *
-from sqlalchemy.testing.suite.test_insert import *
-from sqlalchemy.testing.suite.test_sequence import *
-from sqlalchemy.testing.suite.test_results import *
-from sqlalchemy.testing.suite.test_update_delete import *
-from sqlalchemy.testing.suite.test_reflection import *
-from sqlalchemy.testing.suite.test_types import *
