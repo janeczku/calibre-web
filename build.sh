3a9a327957c6966766c13e4b79b20a515fb0cfd9
林檎 <apple@apples-MacBook-Pro.local>
2017-03-05 15:43:56 +0800

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+if [[$PVERSION=2]]; then
 python -m py_compile cps.py
 python -m py_compile cps/book_formats.py
 python -m py_compile cps/db.py
@@ -8,4 +9,18 @@ python -m py_compile cps/helper.py
 python -m py_compile cps/ub.py
 python -m py_compile cps/uploader.py
 python -m py_compile cps/web.py
-python -m py_compile cps.py    
+python -m py_compile cps.py 
+fi
+f [[$PVERSION=3]]; then
+python3.6 -m py_compile cps.py
+python3.6 -m py_compile cps/book_formats.py
+python3.6 -m py_compile cps/db.py
+python3.6 -m py_compile cps/epub.py
+python3.6 -m py_compile cps/fb2.py
+python3.6 -m py_compile cps/helper.py
+python3.6 -m py_compile cps/ub.py
+python3.6 -m py_compile cps/uploader.py
+python3.6 -m py_compile cps/web.py
+python3.6 -m py_compile cps.py 
+fi
+  
