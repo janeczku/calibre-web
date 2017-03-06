26438f97ab54700136dadda558b014318e073426
OzzieIsaacs <matthias1.knopp@googlemail.com>
2017-03-06 19:19:14 +0100

    Bugfix Filename without unidecode dependency

--- cps/web.py
+++ cps/web.py
@@ -675,7 +675,7 @@ def get_opds_download_link(book_id, format):
         file_name = book.authors[0].name + '-' + file_name
     file_name = helper.get_valid_filename(file_name)
     response = make_response(send_from_directory(os.path.join(config.config_calibre_dir, book.path), data.name + "." + format))
-    response.headers["Content-Disposition"] = "attachment; filename=\"%s.%s\"" % (urllib.quote(file_name.encode('utf8')), format)
+    response.headers["Content-Disposition"] = "attachment; filename*=UTF-8''%s.%s" % (urllib.quote(file_name.encode('utf8')), format)
     return response
 
 
@@ -1283,7 +1283,7 @@ def get_download_link(book_id, format):
             response.headers["Content-Type"] = mimetypes.types_map['.' + format]
         except:
             pass
-        response.headers["Content-Disposition"] = "attachment; filename=\"%s.%s\"" % (urllib.quote(file_name.encode('utf-8')), format)
+        response.headers["Content-Disposition"] = "attachment; filename*=UTF-8''%s.%s" % (urllib.quote(file_name.encode('utf-8')), format)
         return response
     else:
         abort(404)
