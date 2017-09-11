8937599ba7b9e990ac2d05b34518300045b9ea85
kyos <carlos@kyos.ninja>
2017-09-11 17:18:11 +0200

    Fixed codacy code control

--- cps/static/js/edit_books.js
+++ cps/static/js/edit_books.js
@@ -208,7 +208,7 @@ $("#search").on("change input.typeahead:selected", function() {
 
 $("#btn-upload-format").on("change", function () {
     var filename = $(this).val();
-    if (filename.substring(3,11) === "fakepath") {
+    if (filename.substring(3, 11) === "fakepath") {
         filename = filename.substring(12);
     } // Remove c:\fake at beginning from localhost chrome
     $("#upload-format").html(filename);
