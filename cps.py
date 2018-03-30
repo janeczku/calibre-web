af5a897d1de9cf86b082e3355ba74a4964e7ccc1
OzzieIsaacs <ozzie.fernandez.isaacs@gmail.com>
2018-03-30 21:20:47 +0200

    Added SSL Options (#416)

--- cps.py
+++ cps.py
@@ -30,10 +30,18 @@ if __name__ == '__main__':
         else:
             web.app.logger.info('Falling back to Tornado')
             # Max Buffersize set to 200MB
-            http_server = HTTPServer(WSGIContainer(web.app),max_buffer_size = 209700000)
+            if web.ub.config.get_config_certfile() and web.ub.config.get_config_keyfile():
+                ssl={"certfile": web.ub.config.get_config_certfile(),
+                     "keyfile": web.ub.config.get_config_keyfile()}
+            else:
+                ssl=None
+            http_server = HTTPServer(WSGIContainer(web.app),
+                            max_buffer_size = 209700000,
+                            ssl_options=ssl)
             http_server.listen(web.ub.config.config_port)
             IOLoop.instance().start()
             IOLoop.instance().close(True)
+
 
     if web.helper.global_task == 0:
         web.app.logger.info("Performing restart of Calibre-web")
