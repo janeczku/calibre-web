781979f9865f256e076ce45b80ab638581ba16f1
dalin <dalin.lin@gmail.com>
2018-11-14 18:14:11 +0800

    Update Simplified Chinese translation. Fix #681 in the case of Simplified Chinese.

--- cps/web.py
+++ cps/web.py
@@ -198,6 +198,11 @@ def get_locale():
             return user.locale
     translations = [item.language for item in babel.list_translations()] + ['en']
     preferred = [x.replace('-', '_') for x in request.accept_languages.values()]
+
+    # In the case of Simplified Chinese, Accept-Language is "zh-CN", while our translation of Simplified Chinese is "zh_Hans_CN". 
+    # TODO: This is Not a good solution, should be improved.
+    if "zh_CN" in preferred:
+        return "zh_Hans_CN"
     return negotiate_locale(preferred, translations)
 
 
