31e7eb5414c1a2afdd8afaccab5e8a42a7529f58
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2019-02-03 16:38:27 +0100

    Fixes Google Drive Improved user guidance for Google Drive Improved Errorhandling for Google Drive Update french translation Added ukranian translation Improvements on updater: calibre-web.log1, calibre-web.log2, gdrive_credentials, settings.yaml, gdrive.db, .git are no longer deleted upon update

--- cps/helper.py
+++ cps/helper.py
@@ -427,6 +427,8 @@ def delete_book(book, calibrepath, book_format):
 def get_book_cover(cover_path):
     if ub.config.config_use_google_drive:
         try:
+            if not web.is_gdrive_ready():
+                return send_from_directory(os.path.join(os.path.dirname(__file__), "static"), "generic_cover.jpg")
             path=gd.get_cover_via_gdrive(cover_path)
             if path:
                 return redirect(path)
@@ -434,7 +436,7 @@ def get_book_cover(cover_path):
                 web.app.logger.error(cover_path + '/cover.jpg not found on Google Drive')
                 return send_from_directory(os.path.join(os.path.dirname(__file__), "static"), "generic_cover.jpg")
         except Exception as e:
-            web.app.logger.error("Error Message: "+e.message)
+            web.app.logger.error("Error Message: " + e.message)
             web.app.logger.exception(e)
             # traceback.print_exc()
             return send_from_directory(os.path.join(os.path.dirname(__file__), "static"),"generic_cover.jpg")
