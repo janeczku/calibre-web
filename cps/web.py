6269a83c3de77f905b37a085c834bac90acf9c59
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2019-03-03 20:51:19 +0100

    Fix mismerge user logon with python3 in opds feed

--- cps/web.py
+++ cps/web.py
@@ -261,7 +261,7 @@ def load_user_from_header(header_val):
 def check_auth(username, password):
     if sys.version_info.major == 3:
         username=username.encode('windows-1252')
-    user = ub.session.query(ub.User).filter(func.lower(ub.User.nickname) == username.lower()).first()
+    user = ub.session.query(ub.User).filter(func.lower(ub.User.nickname) == username.decode('utf-8').lower()).first()
     return bool(user and check_password_hash(user.password, password))
 
 
