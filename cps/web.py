a0a15b4598d372b05cb30c06ac5787dc229b7e9d
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2019-03-10 19:26:39 +0100

    Fix max authors shown

--- cps/web.py
+++ cps/web.py
@@ -709,6 +709,7 @@ def before_request():
     g.allow_registration = config.config_public_reg
     g.allow_upload = config.config_uploading
     g.current_theme = config.config_theme
+    g.config_authors_max = config.config_authors_max
     g.public_shelfes = ub.session.query(ub.Shelf).filter(ub.Shelf.is_public == 1).order_by(ub.Shelf.name).all()
     if not config.db_configured and request.endpoint not in ('basic_configuration', 'login') and '/static/' not in request.path:
         return redirect(url_for('basic_configuration'))
