4b13615224f77713bb5a676cadd2b9fa025ccff2
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2019-05-30 09:19:37 +0200

    Update version Make custom columns visible (#904)

--- cps/updater.py
+++ cps/updater.py
@@ -237,7 +237,7 @@ class Updater(threading.Thread):
         return False
 
     def _stable_version_info(self):
-        return {'version': '0.6.3'} # Current version
+        return {'version': '0.6.4 Beta'} # Current version
 
     def _nightly_available_updates(self, request_method):
         tz = datetime.timedelta(seconds=time.timezone if (time.localtime().tm_isdst == 0) else time.altzone)
