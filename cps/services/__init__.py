3eae2e9c2cab8089e2579139b998498d93e6207f
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2019-08-03 18:24:56 +0200

    Changed text for not found imports

--- cps/services/__init__.py
+++ cps/services/__init__.py
@@ -26,13 +26,13 @@ log = logger.create()
 
 try: from . import goodreads
 except ImportError as err:
-    log.debug("goodreads: %s", err)
+    log.debug("cannot import goodreads, showing authors-metadata will not work: %s", err)
     goodreads = None
 
 
 try: from . import simpleldap as ldap
 except ImportError as err:
-    log.debug("simpleldap: %s", err)
+    log.debug("cannot import simpleldap, logging in with ldap will not work: %s", err)
     ldap = None
 
 
