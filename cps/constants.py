a14d8460ef7da9075986484e68bee97583c7bd9f
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2020-01-21 07:04:18 +0100

    Update version number

--- cps/constants.py
+++ cps/constants.py
@@ -126,7 +126,7 @@ def selected_roles(dictionary):
 BookMeta = namedtuple('BookMeta', 'file_path, extension, title, author, cover, description, tags, series, '
                                   'series_id, languages')
 
-STABLE_VERSION = {'version': '0.6.5 Beta'}
+STABLE_VERSION = {'version': '0.6.5'}
 
 NIGHTLY_VERSION = {}
 NIGHTLY_VERSION[0] = '$Format:%H$'
