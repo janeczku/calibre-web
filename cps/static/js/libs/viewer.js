df7d3d18b60830e0453a6907856a51893c603964
Ozzie Isaacs <ozzie.fernandez.isaccs@googlemail.com>
2020-02-29 08:33:09 +0100

    pdf reader

--- cps/static/js/libs/viewer.js
+++ cps/static/js/libs/viewer.js
@@ -4372,7 +4372,7 @@ var defaultOptions = {
     kind: OptionKind.WORKER
   },
   workerSrc: {
-    value: '../build/pdf.worker.js',
+    value: '../../../static/js/libs/pdf.worker.js',
     kind: OptionKind.WORKER
   }
 };
