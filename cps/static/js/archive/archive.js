f243515261acc8cf5a52d329eda3900252b2c142
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2020-03-14 07:15:15 +0100

    Fix #950 (sorting order in comics)

--- cps/static/js/archive/archive.js
+++ cps/static/js/archive/archive.js
@@ -40,7 +40,8 @@ function alphanumCase(a, b) {
 
         while (i = (j = t.charAt(x++)).charCodeAt(0)) {
             var m = (i === 46 || (i >= 48 && i <= 57));
-            if (m !== n) {
+            // Compare has to be with != otherwise fails
+            if (m != n) {
                 tz[++y] = "";
                 n = m;
             }
@@ -55,7 +56,8 @@ function alphanumCase(a, b) {
     for (var x = 0; aa[x] && bb[x]; x++) {
         if (aa[x] !== bb[x]) {
             var c = Number(aa[x]), d = Number(bb[x]);
-            if (c === aa[x] && d === bb[x]) {
+            // Compare has to be with == otherwise fails
+            if (c == aa[x] && d == bb[x]) {
                 return c - d;
             } else {
                 return (aa[x] > bb[x]) ? 1 : -1;
