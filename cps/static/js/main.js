38abdbe12050a92d7bf6b668cb52be4d104fa164
Ghighi Eftimie <ghyyy@ymail.com>
2020-10-10 17:51:46 +0300

    fix for #981

--- cps/static/js/main.js
+++ cps/static/js/main.js
@@ -187,6 +187,19 @@ $(function() {
         $(".load-more .row").isotope( "appended", $(data), null );
     });
 
+    // fix for infinite scroll on CaliBlur Theme (#981)
+    if ($(".load-more .row").length && $("body").hasClass("blur")) {
+        $(".col-sm-10").bind("scroll", function () {
+            if (
+                $(this).scrollTop() + $(this).innerHeight() >=
+                $(this)[0].scrollHeight
+            ) {
+                $loadMore.infiniteScroll("loadNextPage");
+                window.history.replaceState({}, null, $loadMore.infiniteScroll('getAbsolutePath')+1)
+            }
+        });
+    }
+
     $("#restart").click(function() {
         $.ajax({
             dataType: "json",
