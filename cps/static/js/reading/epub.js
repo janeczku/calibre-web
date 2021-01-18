486fde8ad0b18e7516849be9a782f8f956f422fc
Samuel Asvanyi <samoasvanyi@gmail.com>
2021-01-18 22:37:52 +0100

    Update Ebook reader

--- cps/static/js/reading/epub.js
+++ cps/static/js/reading/epub.js
@@ -1,6 +1,7 @@
 /* global $, calibre, EPUBJS, ePubReader */
 
 var reader;
+var fontSize = 100;
 
 (function() {
     "use strict";
@@ -10,8 +11,57 @@ var reader;
 
     reader = ePubReader(calibre.bookUrl, {
         restore: true,
-        bookmarks: calibre.bookmark ? [calibre.bookmark] : []
+        bookmarks: calibre.bookmark ? [calibre.bookmark] : [],
+        renderOptions: {
+            ignoreClass: "annotator-hl",
+            width: "100%",
+            height: "100%",
+            spread: "none"
+        }
     });
+
+    reader.rendition.themes.default({
+        h1: {
+            "font-size": "32px",
+            "color": "#000000",
+        },
+        p: {
+            "font-family": "Merriweather",
+            "font-size": "16px",
+            "margin": "20px !important",
+            "line-height": "26px !important",
+            "word-wrap": "break-word !important",
+            "overflow-wrap": "break-word"
+        },
+    });
+
+    reader.adjustFontSize = function(e) {
+        var interval = 1;
+        var PLUS = 187;
+        var MINUS = 189;
+        var ZERO = 48;
+        var MOD = (e.ctrlKey || e.metaKey || e.shiftKey );
+    
+        if(e.keyCode == PLUS && MOD) {
+            e.preventDefault();
+            var newFontSize = fontSize + interval;
+            this.rendition.themes.fontSize(newFontSize + "%");
+            fontSize = newFontSize;
+        }
+    
+        if(e.keyCode == MINUS && MOD){
+            e.preventDefault();
+            var newFontSize = fontSize - interval;
+            this.rendition.themes.fontSize(newFontSize + "%");
+            fontSize = newFontSize;
+        }
+    
+        if(e.keyCode == ZERO && MOD){
+            e.preventDefault();
+            this.rendition.themes.fontSize("100%");
+            fontSize = 100;
+        }
+    };
 
     if (calibre.useBookmarks) {
         reader.on("reader:bookmarked", updateBookmark.bind(reader, "add"));
