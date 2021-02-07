60497c60c1c16b9482cc5f0b576ee672729bb687
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2021-02-07 09:20:37 +0100

    Update creating Home_Config variable for packaging support, added .HOMEDIR to ignored filename during update

--- cps/updater.py
+++ cps/updater.py
@@ -227,7 +227,7 @@ class Updater(threading.Thread):
             os.sep + 'vendor', os.sep + 'calibre-web.log', os.sep + '.git', os.sep + 'client_secrets.json',
             os.sep + 'gdrive_credentials', os.sep + 'settings.yaml', os.sep + 'venv', os.sep + 'virtualenv',
             os.sep + 'access.log', os.sep + 'access.log1', os.sep + 'access.log2',
-            os.sep + '.calibre-web.log.swp', os.sep + '_sqlite3.so'
+            os.sep + '.calibre-web.log.swp', os.sep + '_sqlite3.so', os.sep + 'cps' + os.sep + '.HOMEDIR'
         )
         additional_path = self.is_venv()
         if additional_path:
