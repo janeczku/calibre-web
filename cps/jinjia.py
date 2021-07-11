cb6d366576538f8d2c0638ef0487e1403ad805c8
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2021-07-11 12:52:35 +0200

    Fix #2048 (Display book title in reader)

--- cps/jinjia.py
+++ cps/jinjia.py
@@ -113,10 +113,7 @@ def yesno(value, yes, no):
 
 @jinjia.app_template_filter('formatfloat')
 def formatfloat(value, decimals=1):
-    formatedstring = '%d' % value
-    if (value % 1) != 0:
-        formatedstring = ('%s.%d' % (formatedstring, (value % 1) * 10**decimals)).rstrip('0')
-    return formatedstring
+    return ('{0:.' + str(decimals) + 'g}').format(value)
 
 
 @jinjia.app_template_filter('formatseriesindex')
