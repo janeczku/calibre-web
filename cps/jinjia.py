59be09d6776647fdee31915fb994390eb099fd08
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2021-07-12 14:04:23 +0200

    #2052 (wrong series index shown for series_index >=100)

--- cps/jinjia.py
+++ cps/jinjia.py
@@ -114,7 +114,7 @@ def yesno(value, yes, no):
 @jinjia.app_template_filter('formatfloat')
 def formatfloat(value, decimals=1):
     value = 0 if not value else value
-    return ('{0:.' + str(decimals) + 'g}').format(value)
+    return ('{0:.' + str(decimals) + 'f}').format(value).rstrip('0').rstrip('.')
 
 
 @jinjia.app_template_filter('formatseriesindex')
