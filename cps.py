7cff2a4d09983c73ef4b0c21ad38ca5fed81bdb9
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2021-12-20 20:43:28 +0100

    Add monkey.patch for gevent

--- cps.py
+++ cps.py
@@ -16,6 +16,11 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
+try:
+    from gevent import monkey
+    monkey.patch_all()
+except ImportError:
+    pass
 
 import sys
 import os
