5d64d2fa23670175459e63aa78987aca81027cd8
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2022-04-04 13:58:47 +0200

    Fix for missing "query" entry in flask_session

--- cps/web.py
+++ cps/web.py
@@ -383,7 +383,7 @@ def render_books_list(data, sort_param, book_id, page):
         offset = int(int(config.config_books_per_page) * (page - 1))
         return render_search_results(term, offset, order, config.config_books_per_page)
     elif data == "advsearch":
-        term = json.loads(flask_session['query'])
+        term = json.loads(flask_session.get('query', '{}'))
         offset = int(int(config.config_books_per_page) * (page - 1))
         return render_adv_search_results(term, offset, order, config.config_books_per_page)
     else:
@@ -1556,7 +1556,7 @@ def login():
                     config.config_is_initial = False
                     return redirect_back(url_for("web.index"))
                 else:
-                    log.warning('Login failed for user "%s" IP-address: %s', form['username'], ip_address)
+                    log.warning('Login failed for user "{}" IP-address: {}'.format(form['username'], ip_address))
                     flash(_(u"Wrong Username or Password"), category="error")
 
     next_url = request.args.get('next', default=url_for("web.index"), type=str)
