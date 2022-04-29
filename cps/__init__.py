50fdf9bdb77783814ad3c4f11e7165ede44b5d39
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2022-04-29 18:16:37 +0200

    Bugfix start as program

--- cps/__init__.py
+++ cps/__init__.py
@@ -19,6 +19,8 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
+__package__ = "cps"
+
 import sys
 import os
 import mimetypes
