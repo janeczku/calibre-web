cde51e743a643b312f2760195ac4195962818f6d
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2022-05-01 12:36:35 +0200

    Removed duplicate Werkzeug entry in about page Code cosmetics

--- cps/about.py
+++ cps/about.py
@@ -25,7 +25,6 @@ import platform
 import sqlite3
 from collections import OrderedDict
 
-import werkzeug
 import flask
 import flask_login
 import jinja2
@@ -37,41 +36,40 @@ from .render_template import render_title_template
 
 about = flask.Blueprint('about', __name__)
 
-ret = dict()
-req = dep_check.load_dependencys(False)
-opt = dep_check.load_dependencys(True)
+modules = dict()
+req = dep_check.load_dependencies(False)
+opt = dep_check.load_dependencies(True)
 for i in (req + opt):
-    ret[i[1]] = i[0]
-
-if constants.NIGHTLY_VERSION[0] == "$Format:%H$":
-    calibre_web_version = constants.STABLE_VERSION['version']
-else:
-    calibre_web_version = (constants.STABLE_VERSION['version'] + ' - '
-                           + constants.NIGHTLY_VERSION[0].replace('%', '%%') + ' - '
-                           + constants.NIGHTLY_VERSION[1].replace('%', '%%'))
-
-if getattr(sys, 'frozen', False):
-    calibre_web_version += " - Exe-Version"
-elif constants.HOME_CONFIG:
-    calibre_web_version += " - pyPi"
-
-_VERSIONS = OrderedDict(
-    Platform='{0[0]} {0[2]} {0[3]} {0[4]} {0[5]}'.format(platform.uname()),
-    Python=sys.version,
-    Calibre_Web=calibre_web_version,
-    Werkzeug=werkzeug.__version__,
-    Jinja2=jinja2.__version__,
-    pySqlite=sqlite3.version,
-    SQLite=sqlite3.sqlite_version,
-)
-_VERSIONS.update(ret)
-_VERSIONS.update(uploader.get_versions())
+    modules[i[1]] = i[0]
+modules['Jinja2'] = jinja2.__version__
+modules['pySqlite'] = sqlite3.version
+modules['SQLite'] = sqlite3.sqlite_version
+sorted_modules = OrderedDict((sorted(modules.items(), key=lambda x: x[0].casefold())))
 
 
 def collect_stats():
-    _VERSIONS['ebook converter'] = converter.get_calibre_version()
-    _VERSIONS['unrar'] = converter.get_unrar_version()
-    _VERSIONS['kepubify'] = converter.get_kepubify_version()
+    if constants.NIGHTLY_VERSION[0] == "$Format:%H$":
+        calibre_web_version = constants.STABLE_VERSION['version']
+    else:
+        calibre_web_version = (constants.STABLE_VERSION['version'] + ' - '
+                               + constants.NIGHTLY_VERSION[0].replace('%', '%%') + ' - '
+                               + constants.NIGHTLY_VERSION[1].replace('%', '%%'))
+
+    if getattr(sys, 'frozen', False):
+        calibre_web_version += " - Exe-Version"
+    elif constants.HOME_CONFIG:
+        calibre_web_version += " - pyPi"
+
+    _VERSIONS = {'Calibre Web': calibre_web_version}
+    _VERSIONS.update(OrderedDict(
+        Python=sys.version,
+        Platform='{0[0]} {0[2]} {0[3]} {0[4]} {0[5]}'.format(platform.uname()),
+    ))
+    _VERSIONS.update(uploader.get_magick_version())
+    _VERSIONS['Unrar'] = converter.get_unrar_version()
+    _VERSIONS['Ebook converter'] = converter.get_calibre_version()
+    _VERSIONS['Kepubify'] = converter.get_kepubify_version()
+    _VERSIONS.update(sorted_modules)
     return _VERSIONS
 
 
@@ -80,7 +78,7 @@ def collect_stats():
 def stats():
     counter = calibre_db.session.query(db.Books).count()
     authors = calibre_db.session.query(db.Authors).count()
-    categorys = calibre_db.session.query(db.Tags).count()
+    categories = calibre_db.session.query(db.Tags).count()
     series = calibre_db.session.query(db.Series).count()
     return render_title_template('stats.html', bookcounter=counter, authorcounter=authors, versions=collect_stats(),
-                                 categorycounter=categorys, seriecounter=series, title=_(u"Statistics"), page="stat")
+                                 categorycounter=categories, seriecounter=series, title=_(u"Statistics"), page="stat")
