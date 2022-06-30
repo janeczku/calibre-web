e3bad7ea8a92f36bd41437007bbfee2713378f68
Yip Rui Fung <rf@yrf.me>
2022-07-01 01:36:53 +0800

    Improvements to reverse proxy header login to support reverse proxy auth via CloudFlare Access
    - Allow for matching against username and email
    - Add support for parsing and validating a JWT token using public key obtained from a JWKS endpoint.

--- cps/usermanagement.py
+++ cps/usermanagement.py
@@ -20,11 +20,15 @@ import base64
 import binascii
 from functools import wraps
 
+import jwt
+from jwt import PyJWKClient, PyJWTError
 from sqlalchemy.sql.expression import func
 from werkzeug.security import check_password_hash
 from flask_login import login_required, login_user
 
 from . import lm, ub, config, constants, services
+
+jwks_client = PyJWKClient(config.config_reverse_proxy_jwt_jwks_url)
 
 
 def login_required_if_no_ano(func):
@@ -41,6 +45,21 @@ def _fetch_user_by_name(username):
     return ub.session.query(ub.User).filter(func.lower(ub.User.name) == username.lower()).first()
 
 
+def _fetch_user_by_name_or_email(query):
+    if '@' in query:
+        return ub.session.query(ub.User).filter(
+            (func.lower(ub.User.name) == query.lower()) or (func.lower(ub.User.email) == query.lower())).first()
+    else:
+        return _fetch_user_by_name(query)
+
+
+def _get_jwt_signing_key(token):
+    global jwks_client
+    if jwks_client.uri != config.config_reverse_proxy_jwt_jwks_url:
+        jwks_client = PyJWKClient(config.config_reverse_proxy_jwt_jwks_url)
+    return jwks_client.get_signing_key_from_jwt(token)
+
+
 @lm.user_loader
 def load_user(user_id):
     return ub.session.query(ub.User).filter(ub.User.id == int(user_id)).first()
@@ -52,11 +71,29 @@ def load_user_from_request(request):
         rp_header_name = config.config_reverse_proxy_login_header_name
         if rp_header_name:
             rp_header_username = request.headers.get(rp_header_name)
-            if rp_header_username:
-                user = _fetch_user_by_name(rp_header_username)
+            if rp_header_username and not config.config_reverse_proxy_header_login_uses_jwt:
+                user = _fetch_user_by_name(rp_header_username)\
+                    if not config.config_check_reverse_proxy_header_login_email \
+                    else _fetch_user_by_name_or_email(rp_header_username)
                 if user:
                     login_user(user)
                     return user
+            elif rp_header_username and config.config_reverse_proxy_header_login_uses_jwt:
+                try:
+                    signing_key = _get_jwt_signing_key(rp_header_username)
+                    decoded_token = jwt.decode(rp_header_username, signing_key.key,
+                                               algorithms=['RS256'],
+                                               audience=config.config_reverse_proxy_jwt_audience or None,
+                                               issuer=config.config_reverse_proxy_jwt_issuer or None)
+                    token_username = decoded_token[config.config_reverse_proxy_jwt_id_claim]
+                    user = _fetch_user_by_name(token_username) \
+                        if not config.config_check_reverse_proxy_header_login_email \
+                        else _fetch_user_by_name_or_email(token_username)
+                    if user:
+                        login_user(user)
+                        return user
+                except PyJWTError:
+                    pass
 
     auth_header = request.headers.get("Authorization")
     if auth_header:
