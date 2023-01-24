51f43ffe492da362e05ad02b18fd1128e50d7cf0
Petipopotam <43313692+Petipopotam@users.noreply.github.com>
2023-01-24 10:51:48 +0100

    CSP invalid syntax
    CSP had some "cosmetic" errors
    
    Before : default-src 'self'  'unsafe-inline' 'unsafe-eval'; font-src 'self' data:; img-src 'self' data: style-src-elem 'self' blob: 'unsafe-inline';object-src: 'none';
    After :    default-src 'self'  'unsafe-inline' 'unsafe-eval'; font-src 'self' data:;  img-src 'self' data:; style-src-elem 'self' blob: 'unsafe-inline'; object-src 'none';

--- cps/web.py
+++ cps/web.py
@@ -85,14 +85,14 @@ def add_security_headers(resp):
     csp += " 'unsafe-inline' 'unsafe-eval'; font-src 'self' data:; img-src 'self'"
     if request.path.startswith("/author/") and config.config_use_goodreads:
         csp += " images.gr-assets.com i.gr-assets.com s.gr-assets.com"
-    csp += " data:"
+    csp += " data:;"
     if request.endpoint == "edit-book.show_edit_book" or config.config_use_google_drive:
         csp += " *;"
     elif request.endpoint == "web.read_book":
         csp += " style-src-elem 'self' blob: 'unsafe-inline';"
     else:
         csp += ";"
-    csp += "object-src: 'none';"
+    csp += " object-src 'none';"
     resp.headers['Content-Security-Policy'] = csp
     resp.headers['X-Content-Type-Options'] = 'nosniff'
     resp.headers['X-Frame-Options'] = 'SAMEORIGIN'
