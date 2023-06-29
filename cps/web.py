f18477e02c17e2c9f2544277a9bdca712e799e2e
Blondel MONDESIR <16546989+deldesir@users.noreply.github.com>
2023-06-29 16:41:44 -0400

    style: add missing positional argument

--- cps/web.py
+++ cps/web.py
@@ -1555,7 +1555,7 @@ def read_book(book_id, book_format):
             if book_format.lower() == fileExt:
                 entries = calibre_db.get_filtered_book(book_id)
                 log.debug("Start video watching for %d", book_id)
-                return serve_book.__closure__[0].cell_contents(book_id, book_format.lower())
+                return serve_book.__closure__[0].cell_contents(book_id, book_format.lower(), anyname="")
         for fileExt in ["cbr", "cbt", "cbz"]:
             if book_format.lower() == fileExt:
                 all_name = str(book_id)
