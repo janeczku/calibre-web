741a4debfa27b375ed23ef2e17c72655fae40f46
Blondel MONDESIR <blondel.md@gmail.com>
2024-02-12 14:59:46 +0000

    UPDATE paths instead of REPLACE
    Use UPDATE instead of REPLACE to avoid having new lines created

--- cps/tasks/download.py
+++ cps/tasks/download.py
@@ -97,7 +97,7 @@ class TaskDownload(CalibreTask):
                         self.message = f"Successfully downloaded {self.media_url_link} to <br><br>{file_downloaded}"
                         new_video_path = response.json()["new_book_path"]
                         new_video_path = next((os.path.join(new_video_path, file) for file in os.listdir(new_video_path) if file.endswith((".webm", ".mp4"))), None)
-                        conn.execute("REPLACE INTO media (webpath, path) VALUES (?, ?)", (self.media_url, new_video_path))
+                        conn.execute("UPDATE media SET path = ? WHERE webpath = ?", (new_video_path, self.media_url))
                         self.progress = 1.0
                     else:
                         log.error("Failed to send the requested file to %s", self.original_url)
