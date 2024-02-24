9eb21a1fccfa7861ff00166205ad23b2f936149e
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2024-02-24 14:37:34 +0100

    fix embed metadata, split library and download file / convert files

--- cps/helper.py
+++ cps/helper.py
@@ -1007,10 +1007,17 @@ def do_calibre_export(book_id, book_format, ):
         tmp_dir = get_temp_dir()
         calibredb_binarypath = get_calibre_binarypath("calibredb")
         temp_file_name = str(uuid4())
-        opf_command = [calibredb_binarypath, 'export', '--dont-write-opf', '--with-library', config.config_calibre_dir,
+        my_env = os.environ.copy()
+        if config.config_calibre_split:
+            my_env['CALIBRE_OVERRIDE_DATABASE_PATH'] = os.path.join(config.config_calibre_dir, "metadata.db")
+            library_path = config.config_calibre_split_dir
+        else:
+            library_path = config.config_calibre_dir
+        opf_command = [calibredb_binarypath, 'export', '--dont-write-opf', '--with-library', library_path,
                        '--to-dir', tmp_dir, '--formats', book_format, "--template", "{}".format(temp_file_name),
                        str(book_id)]
-        p = process_open(opf_command, quotes)
+        # CALIBRE_OVERRIDE_DATABASE_PATH
+        p = process_open(opf_command, quotes, my_env)
         _, err = p.communicate()
         if err:
             log.error('Metadata embedder encountered an error: %s', err)
