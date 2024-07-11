b0c422e7a10430b35d99a98c806f69eb698793a0
Blondel MONDESIR <blondel.md@gmail.com>
2024-07-11 11:49:06 -0400

    Import move

--- cps/editbooks.py
+++ cps/editbooks.py
@@ -23,7 +23,7 @@
 import os
 from datetime import datetime
 import json
-from shutil import copyfile
+from shutil import copyfile, move
 from uuid import uuid4
 from markupsafe import escape, Markup  # dependency of flask
 from functools import wraps
