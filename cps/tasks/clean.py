f5eb4eeaaee5a1c23f63d0d3e3fdf5c4e0d87a93
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2024-07-17 19:59:15 +0200

    Clean expired sessions (also old sessions without expiry date)

--- cps/tasks/clean.py
+++ cps/tasks/clean.py
@@ -16,31 +16,48 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from urllib.request import urlopen
+import datetime
 
 from flask_babel import lazy_gettext as N_
+from sqlalchemy.sql.expression import or_
 
-from cps import logger, file_helper
+from cps import logger, file_helper, ub
 from cps.services.worker import CalibreTask
 
 
-class TaskDeleteTempFolder(CalibreTask):
+class TaskClean(CalibreTask):
     def __init__(self, task_message=N_('Delete temp folder contents')):
-        super(TaskDeleteTempFolder, self).__init__(task_message)
+        super(TaskClean, self).__init__(task_message)
         self.log = logger.create()
+        self.app_db_session = ub.get_new_session_instance()
 
     def run(self, worker_thread):
+        # delete temp folder
         try:
             file_helper.del_temp_dir()
         except FileNotFoundError:
             pass
         except (PermissionError, OSError) as e:
             self.log.error("Error deleting temp folder: {}".format(e))
+        # delete expired session keys
+        self.log.debug("Deleted expired session_keys" )
+        expiry = int(datetime.datetime.now().timestamp())
+        try:
+            self.app_db_session.query(ub.User_Sessions).filter(or_(ub.User_Sessions.expiry < expiry,
+                                                               ub.User_Sessions.expiry == None)).delete()
+            self.app_db_session.commit()
+        except Exception as ex:
+            self.log.debug('Error deleting expired session keys: ' + str(ex))
+            self._handleError('Error deleting expired session keys: ' + str(ex))
+            self.app_db_session.rollback()
+            return
+
         self._handleSuccess()
+        self.app_db_session.remove()
 
     @property
     def name(self):
-        return "Delete Temp Folder"
+        return "Clean up"
 
     @property
     def is_cancellable(self):
