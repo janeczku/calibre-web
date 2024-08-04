d3233b4b9cf4ec540ff31fba787865cb95810ae3
Ozzie Isaacs <ozzie.fernandez.isaacs@googlemail.com>
2024-08-04 16:28:49 +0200

    Prepare release

--- cps/constants.py
+++ cps/constants.py
@@ -175,7 +175,7 @@ BookMeta = namedtuple('BookMeta', 'file_path, extension, title, author, cover, d
                                   'series_id, languages, publisher, pubdate, identifiers')
 
 # python build process likes to have x.y.zbw -> b for beta and w a counting number
-STABLE_VERSION = {'version': '0.6.23b'}
+STABLE_VERSION = {'version': '0.6.23'}
 
 NIGHTLY_VERSION = dict()
 NIGHTLY_VERSION[0] = '$Format:%H$'
