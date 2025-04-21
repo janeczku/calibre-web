25672138d8951fc3925fb7f3e91f1c716d537dea
Ryan C. Scott <ryan@5pmcasual.com>
2025-04-20 23:58:46 -0700

    Initial implementation of section progress indicator

--- cps/static/js/reading/epub.js
+++ cps/static/js/reading/epub.js
@@ -70,8 +70,24 @@ var reader;
         }
         make_locations.then(()=>{
             reader.rendition.on('relocated', (location)=>{
-                let percentage = Math.round(location.end.percentage*100);
-                progressDiv.textContent=percentage+"%";
+                const spineItem = reader.book.spine.get(location.start.index);
+                const baseCfi = spineItem.cfiBase;
+                const allLocations = reader.book.locations._locations;
+
+                const sectionStartCfi = allLocations.find(cfi => cfi.includes(baseCfi));
+                const sectionEndCfi = allLocations.findLast(cfi => cfi.includes(baseCfi));
+
+                const sectionStartNormalized = reader.book.locations.percentageFromCfi(sectionStartCfi);
+                const sectionEndNormalized = reader.book.locations.percentageFromCfi(sectionEndCfi);
+                const sectionBookProportion = sectionEndNormalized - sectionStartNormalized;
+
+                const bookNormalized = reader.book.locations.percentageFromCfi(location.start.cfi);
+                const sectionNormalized = (bookNormalized-sectionStartNormalized)/sectionBookProportion;
+
+                const percentageBook = Math.round(bookNormalized*100);
+                const percentageSection = Math.round(sectionNormalized*100);
+
+                progressDiv.textContent=`${percentageSection}% (${percentageBook}% in book)`;
             });
             reader.rendition.reportLocation();
             progressDiv.style.visibility = "visible";
