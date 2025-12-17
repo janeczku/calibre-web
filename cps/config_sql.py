d0e5b1a8f69f883bf5921b203a47a83569a9a6f5
Boris Rybalkin <ribalkin@gmail.com>
2025-12-17 21:40:17 +0000

    fix config

--- cps/config_sql.py
+++ cps/config_sql.py
@@ -196,7 +196,7 @@ class ConfigSQL(object):
         change = False
 
         log.debug("config_binariesdir %s", self.config_binariesdir)
-        if self.config_binariesdir is None:
+        if not self.config_binariesdir:
             change = True
             self.config_binariesdir = autodetect_calibre_binaries()
             self.config_converterpath = autodetect_converter_binary(self.config_binariesdir)
@@ -206,7 +206,7 @@ class ConfigSQL(object):
             self.config_kepubifypath = autodetect_kepubify_binary()
 
         log.debug("config_rarfile_location %s", self.config_rarfile_location)
-        if self.config_rarfile_location is None:
+        if not self.config_rarfile_location:
             change = True
             self.config_rarfile_location = autodetect_unrar_binary()
         if change:
