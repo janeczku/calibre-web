01c4477cd3b1fbd0d1afeb97e52e1f8d62e12793
Asher Max Schweigart <AsherMaximum@gmail.com>
2026-05-15 12:19:33 -0400

    Cleaning up "books" link in sidebar to remove unused params

--- cps/render_template.py
+++ cps/render_template.py
@@ -39,7 +39,7 @@ def get_sidebar_config(kwargs=None):
         content = 'conf' in kwargs
     sidebar = list()
     sidebar.append({"glyph": "glyphicon-book", "text": _('Books'), "link": 'web.index', "id": "new",
-                    "visibility": constants.SIDEBAR_RECENT, 'public': True, "page": "root",
+                    "visibility": constants.SIDEBAR_RECENT, 'public': True, "page": "root", "no_param": True,
                     "show_text": _('Show recent books'), "config_show":False})
     sidebar.append({"glyph": "glyphicon-fire", "text": _('Hot Books'), "link": 'web.books_list', "id": "hot",
                     "visibility": constants.SIDEBAR_HOT, 'public': True, "page": "hot",
